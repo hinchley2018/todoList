f2a8ecc17c88998c26d2f875cbc71a6d38b335a6
Jonathon Hinchley <jhinchley@Jonathons-MacBook-Air.local>
2017-10-15 04:59:59 -0500

    server setup

---
+++ server.js
@@ -0,0 +1,7 @@
+//setup
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var morgan = require('morgan');
+var bodyParse = require('body-parser');
+var methodOverride = require('method-override');
