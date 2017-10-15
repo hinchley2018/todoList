cf1894e7b199c178e864730008f0489133eb0113
Jonathon Hinchley <jhinchley@Jonathons-MacBook-Air.local>
2017-10-15 06:03:16 -0500

    refactor moved mongoose and todo obj to api/index.js because I moved api routing

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //imports
 import express from 'express';
-import mongoose from 'mongoose';
+
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import methodOverride from 'method-override';
