bca22639afeac6d5ae350e8a8e9da4dcc974893a
Jonathon Hinchley <jhinchley@Jonathons-MacBook-Air.local>
2017-10-15 05:09:37 -0500

    config

--- server.js
+++ server.js
@@ -3,5 +3,20 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 var morgan = require('morgan');
-var bodyParse = require('body-parser');
+var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+
+
+//config
+//TODO: check if the tutorial guy is letting us use this db
+mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu');
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev')); //log every request to console
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+app.use(methodOverride());
+
+//listen
+app.listen(8080); //so its the quick and dirty config huh... naughty tutorial guy
+console.log("TodoList app listening on port 8080");
